b7432b426ead611c90c945f80929e28e69b854cd
Ziqi Wang <ziqi.ccc.wang@student.jyu.fi>
2021-10-16 13:06:15 +0300

    exercise 6.7 & 6.8

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,42 +1,13 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { voteAction, createAction } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state.slice().sort((a, b) => b.votes - a.votes))
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    console.log('vote', id)
-    dispatch(voteAction(id))
-  }
-
-  const createNew = (event) => {
-    event.preventDefault()
-    const content=event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createAction(content))
-  }
-  
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form onSubmit={createNew}>
-        <div><input name="anecdote"/></div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteList />
+      <AnecdoteForm />
     </div>
   )
 }
---
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { voteAction } from "../reducers/anecdoteReducer"
+
+const Anecdote = ({ anecdote, handleClick }) =>
+{
+  return (
+    <div className="Anecdote">
+      <div>
+        {anecdote.content}
+      </div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleClick}>vote</button>
+      </div>
+    </div>)
+}
+
+const AnecdoteList = () =>
+{
+  const anecdotes = useSelector(state => state.slice().sort((a, b) => b.votes - a.votes))
+  const dispatch = useDispatch()
+
+  const vote = (id) => dispatch(voteAction(id))
+
+  return (
+    <div className="AList">
+      {anecdotes.map(anecdote =>
+        <Anecdote
+          key={anecdote.id}
+          anecdote={anecdote}
+          handleClick={() => { vote(anecdote.id) }} />)}
+    </div>)
+}
+
+export default AnecdoteList
