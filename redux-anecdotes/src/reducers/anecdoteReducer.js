a94e3cb2ddfac8e3a0083ff2578f69f5dfa35166
Ziqi Wang <ziqi.ccc.wang@student.jyu.fi>
2021-10-19 00:27:09 +0300

    exercise 6.13

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,15 +1,7 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
-const asObject = (anecdote) => {
+const asObject = (anecdote) =>
+{
   return {
     content: anecdote,
     id: getId(),
@@ -17,38 +9,50 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
-const anecdoteReducer = (state = initialState, action) => {
+const anecdoteReducer = (state = [], action) =>
+{
   // console.log('state now: ', state)
   // console.log('action', action)
-  switch(action.type) {
+  switch (action.type)
+  {
     case 'VOTE':
       const id = action.data.id
-      const anecdote = state.find(n=>n.id===id)
-      const changed = { ...anecdote, votes: anecdote.votes+1 }
+      const anecdote = state.find(n => n.id === id)
+      const changed = { ...anecdote, votes: anecdote.votes + 1 }
       return state.map(anecdote => anecdote.id !== id ? anecdote : changed)
     case 'NEW':
-      const content=action.data.content
+      const content = action.data.content
       return [...state, asObject(content)]
+    case 'INIT':
+      return action.data
     default:
       return state
   }
 }
 
-export const voteAction = (id) => {
+export const voteAction = (id) =>
+{
   return {
     type: 'VOTE',
-    data: {id: id}
+    data: { id: id }
   }
 }
 
-export const createAction = (content) => {
+export const createAction = (content) =>
+{
   return {
-    type:'NEW',
-    data:{
-      content:content
+    type: 'NEW',
+    data: {
+      content: content
     }
+  }
+}
+
+export const initAction = (anecdotes) =>
+{
+  return {
+    type: 'INIT',
+    data: anecdotes
   }
 }
 
